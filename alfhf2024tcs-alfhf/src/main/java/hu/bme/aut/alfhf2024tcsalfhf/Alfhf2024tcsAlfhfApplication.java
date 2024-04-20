957009b5eafe1511cf1364778c0d46e59924c956
szfp98 <pal.szabofeltothy@gmail.com>
2024-04-20 23:29:40 +0200

    Create multi-modular project structure for back-end project

---
+++ alfhf2024tcs-alfhf/src/main/java/hu/bme/aut/alfhf2024tcsalfhf/Alfhf2024tcsAlfhfApplication.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.alfhf2024tcsalfhf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Alfhf2024tcsAlfhfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Alfhf2024tcsAlfhfApplication.class, args);
+    }
+
+}
---
+++ alfhf2024tcs-alfhf/src/test/java/hu/bme/aut/alfhf2024tcsalfhf/Alfhf2024tcsAlfhfApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.alfhf2024tcsalfhf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Alfhf2024tcsAlfhfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
